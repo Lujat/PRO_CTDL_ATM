7dc3d6d7b368da071d40c67b999acb9069f5f4e9
DESKTOP-85RQR69\GIGABYTE <trungluat1305@gmail.com>
2026-08-29 12:28:19 +0700

    dang nhap && menu admin

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include<fstream>
 #include<string>
 #include <cstdio>
+#include<conio.h>
 using namespace std;
 
 struct TheTu{
@@ -14,16 +15,52 @@ struct TaiKhoan{
     long long accBalance;
     string currency;
 };
+// DANG NHAP ADMIN 
+bool dangnhapAdmin(){
+    string user;
+    string pin;
 
-void hienthiTheTu()
-{
-    string filename = "TheTu.txt";
-    ifstream file(filename);
-    string line;
-    while(getline(file, line)){
-        cout<< line << endl;
+    cout << "************************" << endl;
+    cout << "*    DANG NHAP ADMIN   *" << endl;
+    cout << "************************" << endl;
+    cout << "User: ";
+    cin >> user;
+    cout <<"Pin: ";
+    char ch;
+    while(true){
+        ch =_getch();
+        if(ch==13){
+            break;
+        }
+        if(ch==8){
+            if(!pin.empty()){
+                pin.pop_back();
+                cout <<"\b \b";
+            }
+        }
+        else{
+            pin += ch;
+            cout <<"*";
+        }
+    }
+    cout << endl;
+    ifstream file("Admin.txt");
+    if(!file){
+        cout<< "ko mo duoc Admin.txt!" << endl;
+        return false;
+    }
+    string userfile;
+    string pinfile;
+    while(file >> userfile >>pinfile){
+        if(userfile == user && pinfile==pin){
+            file.close();
+            cout<<"Dang nhap thanh cong!";
+            return true;
+        }
     }
     file.close();
+    cout << "sai User hoac pin!";
+    return false;
 }
 void themtk(){
     string id;
@@ -79,9 +116,62 @@ void xoatk(){
     rename("NewTheTu.txt", "TheTu.txt");
     cout <<"da xoa va cap nhap thanh cong"<< endl;
 }
+void hienthiTheTu()
+{
+    string filename = "TheTu.txt";
+    ifstream file(filename);
+    string line;
+    while(getline(file, line)){
+        cout<< line << endl;
+    }
+    file.close();
+}
+void menuAdmin(){
+    int chose;
+    do {
+        cout << endl;
+        cout << "*************MENU*************" << endl;
+        cout << "1. Xem danh sach tai khoan" << endl;
+        cout << "2. Them tai khoan" << endl;
+        cout << "3. Xoa tai khoan" << endl;
+        cout << "4. Mo khoa tai khoan" << endl;
+        cout << "5. Thoat" << endl;
+        cout << "******************************" << endl;
+        cout << "Nhap lua chon: ";
+        cin >> chose;
+
+
+        switch (chose) {
+            case 1:
+                hienthiTheTu();
+                break;
+             case 2:
+                themtk();
+                break;
+            case 3:
+                xoatk();
+                break;
+            case 4:
+                cout << "Chuc nang mo khoa tai khoan" << endl;
+                break;
+            case 5:
+                cout << "Thoat!" << endl;
+                break;
+
+            default:
+                cout << "Lua chon khong hop le!" << endl;
+        }
+    } while (chose != 5);
+}    
 int main(){
     //hienthiTheTu();
     //themtk();
-    xoatk();
+    //xoatk();
+    if (dangnhapAdmin()) {
+        menuAdmin();
+    }
+
     return 0;
+    
+
 }
